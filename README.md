07dcf45039e0781ec18062cadb1102ba7cbfe7fc
KetchuppOfficial <dolgov04@list.ru>
2024-10-25 18:04:34 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+# YARVS - Yet Another RISC-V Simulator
+
+## Requirements
+
+The following applications and libraries have to be installed:
+
+- CMake of version 3.21 (or higher)
+- python3
+- python3-venv
+- pip
+
+## How to build
+
+### 0) Make sure you are in the root directory of the project (i.e. YARVS/)
+
+### 1) Installing conan and other python modules this project depends on
+
+```bash
+python3 -m venv .venv
+source .venv/bin/activate
+pip3 install -r requirements.txt
+```
+
+### 2) Build the project
+
+```bash
+conan install . --output-folder=third_party --build=missing -s compiler.cppstd=23
+cmake -B build -DCMAKE_BUILD_TYPE=Release -DCMAKE_TOOLCHAIN_FILE=./third_party/conan_toolchain.cmake
+cmake --build build [--target <tgt>]
+```
---
+++ src/main.cpp
@@ -0,0 +1,43 @@
+#include <exception>
+#include <filesystem>
+
+#include "CLI/CLI.hpp"
+#include "fmt/base.h"
+
+#include "elf_loader.hpp"
+
+int main(int argc, char **argv) try
+{
+    CLI::App app{"YARVS: Yet Another RISC-V Simulator"};
+
+    std::filesystem::path elf_path;
+    app.add_option("--elf", elf_path, "Path to RISC-V executable")
+        ->required()
+        ->check(CLI::ExistingFile);
+
+    CLI11_PARSE(app, argc, argv);
+
+    yarvs::LoadableImage segments{elf_path};
+
+    auto i = 0;
+    fmt::println("ELF file contains {} loadable segments", segments.size());
+    for (auto &seg : segments)
+    {
+        fmt::println("Segment {}", i++);
+        fmt::println("    file size: {} bytes", seg.get_file_size());
+        fmt::println("    memory size: {} bytes", seg.get_memory_size());
+        fmt::println("    virtual address: {:#x}", seg.get_virtual_addr());
+    }
+
+    return 0;
+}
+catch (const std::exception &e)
+{
+    fmt::println("Caught as instance of {}.\nwhat(): {}", typeid(e).name(), e.what());
+    return 1;
+}
+catch (...)
+{
+    fmt::println("Caught an unknown exception");
+    return 1;
+}
