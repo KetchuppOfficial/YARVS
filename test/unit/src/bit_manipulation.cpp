c68efc4ea27badb777c4ae0651369d72d4b38701
KetchuppOfficial <dolgov04@list.ru>
2024-11-05 14:29:35 +0300

    implement tests for get_bit() and sext()

--- test/unit/src/bit_manipulation.cpp
+++ test/unit/src/bit_manipulation.cpp
@@ -60,3 +60,54 @@ TEST(Bits_Manipulation, Get_Bits_R)
     EXPECT_EQ((yarvs::get_bits_r<15, 5, uint16_t>(uint16_t{0b1011011011101011})),
               uint32_t{0b10110110111});
 }
+
+TEST(Bits_Manipulation, Get_Bit)
+{
+    // first bit
+    EXPECT_EQ((yarvs::get_bit<0>(uint16_t{0b1011011011101010})), uint16_t{0});
+
+    // last bit
+    EXPECT_EQ((yarvs::get_bit<15>(uint16_t{0b1011011011101010})), uint16_t{1});
+
+    // middle bit
+    EXPECT_EQ((yarvs::get_bit<7>(uint16_t{0b1011011011101010})), uint16_t{1});
+}
+
+TEST(Bits_Manipulation, Sext)
+{
+    // 64 -> 64
+    EXPECT_EQ((yarvs::sext<64, uint64_t>(uint64_t{0xFFFFFFFFFFFFFFFF})),
+              0xFFFFFFFFFFFFFFFF);
+    EXPECT_EQ((yarvs::sext<64, uint64_t>(uint64_t{0x7FFFFFFFFFFFFFFF})),
+              0x7FFFFFFFFFFFFFFF);
+
+    // 32 -> 64
+    EXPECT_EQ((yarvs::sext<32, uint64_t>(uint32_t{0xFFFFFFFF})),
+              0xFFFFFFFFFFFFFFFF);
+    EXPECT_EQ((yarvs::sext<32, uint64_t>(uint32_t{0x7FFFFFFF})),
+              0x000000007FFFFFFF);
+
+    // 16 -> 64
+    EXPECT_EQ((yarvs::sext<16, uint64_t>(uint16_t{0xFFFF})),
+              0xFFFFFFFFFFFFFFFF);
+    EXPECT_EQ((yarvs::sext<16, uint64_t>(uint16_t{0x7FFF})),
+              0x0000000000007FFF);
+
+    // 8 -> 64
+    EXPECT_EQ((yarvs::sext<8, uint64_t>(uint8_t{0xFF})),
+              0xFFFFFFFFFFFFFFFF);
+    EXPECT_EQ((yarvs::sext<8, uint64_t>(uint8_t{0x7F})),
+              0x000000000000007F);
+
+    // 20 -> 64
+    EXPECT_EQ((yarvs::sext<20, uint64_t>(uint32_t{0xFFFFF})),
+              0xFFFFFFFFFFFFFFFF);
+    EXPECT_EQ((yarvs::sext<20, uint64_t>(uint32_t{0x7FFFF})),
+              0x000000000007FFFF);
+
+    // 12 -> 64
+    EXPECT_EQ((yarvs::sext<12, uint64_t>(uint16_t{0xFFF})),
+              0xFFFFFFFFFFFFFFFF);
+    EXPECT_EQ((yarvs::sext<12, uint64_t>(uint16_t{0x7FF})),
+              0x000000000007FF);
+}
