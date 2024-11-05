9865e053b73b3f0bf23869916c48a8d942bb900c
KetchuppOfficial <dolgov04@list.ru>
2024-11-05 12:10:36 +0300

    implement a concept for valid RISC-V types

--- include/common.hpp
+++ include/common.hpp
@@ -2,6 +2,7 @@
 #define INCLUDE_COMMON_HPP
 
 #include <cstdint>
+#include <concepts>
 #include <climits>
 
 namespace yarvs
@@ -11,6 +12,11 @@ using Byte = uint8_t;
 using HalfWord = uint16_t;
 using Word = uint32_t;
 using DoubleWord = uint64_t;
+
+template<typename T>
+concept riscv_type =
+    std::same_as<T, Byte> || std::same_as<T, HalfWord> ||
+    std::same_as<T, Word> || std::same_as<T, DoubleWord>;
 
 using RawInstruction = uint32_t;
 
