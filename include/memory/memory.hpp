c1eb7a684ecb8597b688dd72132fb2782049af52
KetchuppOfficial <dolgov04@list.ru>
2024-11-12 16:23:52 +0300

    add getter for a pointer to the memory

--- include/memory/memory.hpp
+++ include/memory/memory.hpp
@@ -23,22 +23,25 @@ public:
     T load(DoubleWord addr) const
     {
         // temporary implementation without virtual memory
-        return *reinterpret_cast<const T*>(&physical_mem_[addr]);
+        return *reinterpret_cast<const T *>(ptr(addr));
     }
 
     template<riscv_type T>
     void store(DoubleWord addr, T value)
     {
         // temporary implementation without virtual memory
-        *reinterpret_cast<T*>(&physical_mem_[addr]) = value;
+        *reinterpret_cast<T*>(ptr(addr)) = value;
     }
 
     template<std::forward_iterator It>
     void store(DoubleWord addr, It first, It last)
     {
         using value_type = typename std::iterator_traits<It>::value_type;
-        std::copy(first, last, reinterpret_cast<value_type *>(&physical_mem_[addr]));
+        std::copy(first, last, reinterpret_cast<value_type *>(ptr(addr)));
     }
+
+    const uint8_t *ptr(DoubleWord addr) const { return &physical_mem_[addr]; }
+    uint8_t *ptr(DoubleWord addr) { return &physical_mem_[addr]; }
 
 private:
 
