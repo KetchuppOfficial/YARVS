e9c24e642704309dd9da87162aca8ad8b1916018
KetchuppOfficial <dolgov04@list.ru>
2024-11-13 10:24:35 +0300

    use std:: types

--- include/common.hpp
+++ include/common.hpp
@@ -8,17 +8,17 @@
 namespace yarvs
 {
 
-using Byte = uint8_t;
-using HalfWord = uint16_t;
-using Word = uint32_t;
-using DoubleWord = uint64_t;
+using Byte = std::uint8_t;
+using HalfWord = std::uint16_t;
+using Word = std::uint32_t;
+using DoubleWord = std::uint64_t;
 
 template<typename T>
 concept riscv_type =
     std::same_as<T, Byte> || std::same_as<T, HalfWord> ||
     std::same_as<T, Word> || std::same_as<T, DoubleWord>;
 
-using RawInstruction = uint32_t;
+using RawInstruction = std::uint32_t;
 
 constexpr auto kXLen = sizeof(DoubleWord) * CHAR_BIT;
 constexpr std::size_t kOpcodeBitLen = 7;
